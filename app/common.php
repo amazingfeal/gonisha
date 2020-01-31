dba300c11a3aa6b915010911da3cb288960a5777
Feal <48620965+amazingfeal@users.noreply.github.com>
2020-01-31 23:39:40 +0330

    123
    1213

--- app/common.php
+++ app/common.php
@@ -2,10 +2,11 @@
 
 class Common {
     public static function URL()
-        $adress=$_SERVER['REQUEST_URI'];
-        echo $adress;
+    {
 
+
+        $adress = $_SERVER['REQUEST_URI'];
+        echo $adress;
+    }
 
 }
-
-echo Common::URL()
