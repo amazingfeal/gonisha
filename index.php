8c9d36b2804fa990ed01563048f5715c6a5091f4
Nero <ahmadkazama77@gmail.com>
2020-01-31 23:19:34 +0330

    kj,jk

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once 'app/autoload.php';
+
+
+
+$route = new route();
+$route->get($mainRoute);
---
+++ routes/route.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once 'system/autoload.php';
+
+class route
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function get($mainRoute) {
+
+    }
+
+}
