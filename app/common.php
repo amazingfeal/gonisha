24319e92c1aca602b88748d39bda74d865fd513b
Feal <48620965+amazingfeal@users.noreply.github.com>
2020-02-01 00:09:04 +0330

    11
    1

--- app/common.php
+++ app/common.php
@@ -2,11 +2,13 @@
 
 class Common {
 
-    public static function URL()
+    public static function url()
     {
 
-        $adress = $_SERVER['REQUEST_URI'];
+        $adress = "$_SERVER['REQUEST_URI']";
 
+        $url = str_replace("/gonisha/", "/", $adress);
+        return $url;
     }
 
 }
