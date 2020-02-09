b25ec3a4730cf3be1d1b9ee4d4faf759b9b97b5a
Nero <ahmadkazama77@gmail.com>
2020-02-09 23:27:14 +0330

    Create db.php

---
+++ app/database/db.php
@@ -0,0 +1,22 @@
+<?php
+
+class DataBase {
+
+    private $connection;
+
+    function __construct() {
+
+        global $config;
+        $host = $config['db']['host'];
+        $user = $config['db']['user'];
+        $pass = $config['db']['pass'];
+        $name = $config['db']['name'];
+
+        $this->connection = new mysql($host, $user, $pass, $name);
+        if($this->connection->connect_error) {
+            echo "Connection failed: " . $this->connection->connect_error;
+            exit;
+        }
+    }
+
+}
